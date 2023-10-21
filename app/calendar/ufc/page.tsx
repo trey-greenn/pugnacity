1ac4239527345014f8a67f2d33e979e52dc8f4d2
trey-greenn <w.treygreen@gmail.com>
2023-10-21 07:01:59 -0700

    calendar/schedule

---
+++ app/calendar/ufc/page.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+
+const page = () => {
+    return(
+        <div>
+            <title>UFC Schedule - 2023</title>
+            <h1>UFC Schedule - 2023</h1>
+            <table>
+                <tr>
+                    <th>Date</th>
+                    <th>Time</th>
+                    <th>TV</th>
+                    <th>Event</th>
+                    <th>Location</th>
+                </tr>
+
+                <tr>
+                    <td> Oct 21</td>
+                    <td> 7:00 AM</td>
+                    <td> ESPN +</td>
+                    <td> ufc 294: MAKHACHEV VS VOLKANOVSKI 2</td>
+                    <td> Etihad Arena, Abu Dhabi, United Arab Emirates</td>
+
+
+                </tr>
+
+
+            </table>
+
+        </div>
+
+    )
+
+
+}
+export default page
---
+++ app/ufc/ufc-294/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head  from 'next/head';
+
+const page = () => {
+  return (
+    <div>
+        <title>UFC 294: Makhachev vs Volkanovski</title>
+        <meta name='description' content='UFC 294: Makhachev vs Volkanovski, Kamaura Usman vs Khamzat Chimaev, Magomed Ankalaev vs Johnny Walker, Ikram Alskerov vs Warlley Alves, Said Nurmagomedov vs Muin Gafurov'/>
+        <div>
+            <a className="font-black">Hi</a>
+        </div>
+    </div>
+  )
+}
+
+export default page
