e001122d0e0a871ab6fb6c22cf4d6d4048ad510f
trey-greenn <w.treygreen@gmail.com>
2023-11-02 16:49:56 -0700

    home page play

---
+++ component/header.tsx
@@ -0,0 +1,22 @@
+
+
+export default function Example() {
+
+  return (
+    <header className="bg-black text-center mt-10">
+      <nav>
+      <div className="header">
+        <a href="#default" className="logo text-white">Pugnacity</a>
+        <div className="header-right">
+          <a className="active" href="#home">CALENDAR</a>
+          <a href="#contact">HEDGER</a>
+          <a href="#about">MATCH UPS</a>
+        </div>
+</div>
+
+      </nav>
+  
+    
+    </header>
+  )
+}
